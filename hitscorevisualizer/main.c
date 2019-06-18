f2f1a0503440f521798b2bdcaabddb7d18523f08
Adam ? <adamznow@gmail.com>
2019-06-18 00:31:28 -0700

    Added lots of code, including judgements!
    For completion-sake, should download an older version to check to make sure that colors change before checking to see if this works (this could break many things).
    God knows if my structs are even the proper size (for all I know, classes with properties could ruin my day)

--- hitscorevisualizer/main.c
+++ hitscorevisualizer/main.c
@@ -13,6 +13,63 @@
 #include "../beatsaber-hook/shared/inline-hook/inlineHook.h"
 #include "../beatsaber-hook/shared/utils/utils.h"
 
+typedef struct judgement {
+    int threshold;
+    float r;
+    float g;
+    float b;
+    float a;
+    char* text;
+} judgement_t;
+
+typedef struct node
+{
+    judgement_t* value;
+    struct node* next;
+} node_t;
+
+float temp_r = 1;
+float temp_g = 0;
+float temp_b = 0;
+float temp_a = 1;
+node_t* judgements = NULL;
+
+judgement_t* get(node_t* head, int index) {
+    node_t* current = head;
+    int cI = 0;
+    while (cI < index) {
+        if (current == NULL) {
+            return NULL;
+        }
+        current = current->next;
+        cI++;
+    }
+    return current->value;
+}
+
+int size(node_t* head) {
+    int length = 0;
+    node_t* current = head;
+    while (current != NULL) {
+        current = current->next;
+        length++;
+    }
+    return length;
+}
+
+void add(node_t* head, judgement_t* value) {
+    if (head == NULL) {
+        return;
+    }
+    node_t* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = malloc(sizeof(node_t));
+    current->next->value = value;
+    current->next->next = NULL;
+}
+
 typedef struct __attribute__((__packed__)) {
     float r;
     float g;
@@ -31,21 +88,36 @@ typedef struct __attribute__((__packed__)) {
     
 } FlyingScoreEffect;
 
-float temp_r = 1;
-float temp_g = 0;
-float temp_b = 0;
-float temp_a = 1;
+void checkJudgements(FlyingScoreEffect* scorePointer, int score) {
+    log("Checking judgements for score: %i", score);
+    int l = size(judgements);
+    log("Beginning for loop...");
+    judgement_t* best = get(judgements, 0);
+    for (int i = 1; i < l; i++) {
+        if (get(judgements, i)->threshold > score) {
+            break;
+        }
+        best = get(judgements, i);
+        log("Setting new best judgement at index %i with text %s", i, best->text);
+    }
+    log("Setting score effect's color to best color with threshold: %i", best->threshold);
+    scorePointer->color->r = best->r;
+    scorePointer->color->g = best->g;
+    scorePointer->color->b = best->b;
+    scorePointer->color->a = best->a;
+    log("Complete!");
+}
 
 MAKE_HOOK(raw_score_without_multiplier, 0x48C248, void, void* noteCutInfo, void* saberAfterCutSwingRatingCounter, int* beforeCutRawScore, int* afterCutRawScore, int* cutDistanceRawScore) {
-    log("Created RawScoreWithoutMultiplier Hook!");
+    log("Called RawScoreWithoutMultiplier Hook!");
     raw_score_without_multiplier(noteCutInfo, saberAfterCutSwingRatingCounter, beforeCutRawScore, afterCutRawScore, cutDistanceRawScore);
 }
 
-MAKE_HOOK(init_and_present, 0x132307C, void, void* noteCut, int multiplier, float duration, void* targetPos, Color* color, void* saberAfterCutSwingRatingCounter) {
+MAKE_HOOK(init_and_present, 0x132307C, void, FlyingScoreEffect* self, void* noteCut, int multiplier, float duration, void* targetPos, Color* color, void* saberAfterCutSwingRatingCounter) {
     // Placeholder, for now.
-    log("Created InitAndPresent Hook!");
+    log("Called InitAndPresent Hook!");
     log("Attempting to call standard InitAndPresent...");
-    init_and_present(noteCut, multiplier, duration, targetPos, color, saberAfterCutSwingRatingCounter);
+    init_and_present(self, noteCut, multiplier, duration, targetPos, color, saberAfterCutSwingRatingCounter);
     log("Creating score int* pointers!");
     int* beforeCut = malloc(sizeof(int));
     int* afterCut = malloc(sizeof(int));
@@ -53,20 +125,37 @@ MAKE_HOOK(init_and_present, 0x132307C, void, void* noteCut, int multiplier, floa
     log("Attempting to call RawScoreWithoutMultiplier hook...");
     raw_score_without_multiplier(noteCut, saberAfterCutSwingRatingCounter, beforeCut, afterCut, cutDistance);
     log("Completed!");
-    log("RawScore: %i", *(*afterCut) + *(*cutDistance));
+    int score = *afterCut + *cutDistance;
+    log("RawScore: %i", score);
+    log("Checking judgements...");
+    checkJudgements(self, score);
+}
+
+MAKE_HOOK(HandleSaberAfterCutSwingRatingCounterDidChangeEvent, 0x13233DC, void, FlyingScoreEffect* self, void* saberAfterCutSwingRatingCounter, float rating) {
+    log("Called HandleSaberAfterCutSwingRatingCounterDidChangeEvent Hook!");
+    log("Attempting to call standard HandleSaberAfterCutSwingRatingCounterDidChangeEvent...");
+    HandleSaberAfterCutSwingRatingCounterDidChangeEvent(self, saberAfterCutSwingRatingCounter, rating);
+    log("Creating score int* pointers!");
+    int* beforeCut = malloc(sizeof(int));
+    int* afterCut = malloc(sizeof(int));
+    int* cutDistance = malloc(sizeof(int));
+    log("Attempting to call RawScoreWithoutMultiplier hook...");
+    raw_score_without_multiplier(self->noteCutInfo, self->saberAfterCutSwingRatingCounter, beforeCut, afterCut, cutDistance);
+    log("Completed!");
+    int score = *afterCut + *cutDistance;
+    log("RawScore: %i", score);
+    log("Checking judgements...");
+    checkJudgements(self, score);
 }
 
 MAKE_HOOK(manual_update, 0x1323314, void, FlyingScoreEffect* self, float t) {
-    // Lets just test to make sure this one works without running the actual ones that would change color based on hit score
-    log("Attempting to create new pointer to color with self pointer: %i", self);
-    self->color = malloc(sizeof(Color));
-    log("Completed malloc for Color struct!");
-    log("Attemping to set color to: %f, %f, %f", temp_r, temp_g, temp_b);
-    self->color->r = temp_r;
-    self->color->g = temp_g;
-    self->color->b = temp_b;
-    self->color->a = temp_a;
-    log("Set color to: %f, %f, %f", temp_r, temp_g, temp_b);
+    // log("Attemping to set color to: %f, %f, %f, %f", temp_r, temp_g, temp_b, temp_a);
+    // self->color->r = temp_r;
+    // self->color->g = temp_g;
+    // self->color->b = temp_b;
+    // self->color->a = temp_a;
+    // log("Set color to: %f, %f, %f, %f", temp_r, temp_g, temp_b, temp_a);
+    log("Calling ManualUpdate!");
     manual_update(self, t);
 }
 
@@ -79,4 +168,103 @@ __attribute__((constructor)) void lib_main()
     log("Installed InitAndPresent Hook!");
     INSTALL_HOOK(raw_score_without_multiplier);
     log("Installed RawScoreWithoutMultiplier Hook!");
+    INSTALL_HOOK(HandleSaberAfterCutSwingRatingCounterDidChangeEvent);
+    log("Installed HandleSaberAfterCutSwingRatingCounterDidChangeEvent Hook!");
+    // Attempt to add and create judgements
+    log("Attempting to create empty judgements linked list...");
+    judgements = malloc(sizeof(node_t));
+    // Creating Fantastic judgement
+    judgement_t* fantastic = malloc(sizeof(judgement_t));
+    fantastic->r = 1.0f;
+    fantastic->g = 1.0f;
+    fantastic->b = 1.0f;
+    fantastic->a = 1.0f;
+    fantastic->text = "Fantastic\n";
+    fantastic->threshold = 115;
+    add(judgements, fantastic);
+    // Creating Excellent judgement
+    judgement_t* excellent = malloc(sizeof(judgement_t));
+    excellent->r = 0.0f;
+    excellent->g = 1.0f;
+    excellent->b = 0.0f;
+    excellent->a = 1.0f;
+    excellent->text = "<size=80%>Excellent</size>\n";
+    excellent->threshold = 101;
+    add(judgements, excellent);
+    // Creating Great judgement
+    judgement_t* great = malloc(sizeof(judgement_t));
+    great->r = 1.0f;
+    great->g = 0.980392158f;
+    great->b = 0.0f;
+    great->a = 1.0f;
+    great->text = "<size=80%>Great</size>\n";
+    great->threshold = 90;
+    add(judgements, great);
+    // Creating Good judgement
+    judgement_t* good = malloc(sizeof(judgement_t));
+    good->r = 1.0f;
+    good->g = 0.6f;
+    good->b = 0.0f;
+    good->a = 1.0f;
+    good->text = "<size=80%>Good</size>\n";
+    good->threshold = 80;
+    add(judgements, good);
+    // Creating Decent judgement
+    judgement_t* decent = malloc(sizeof(judgement_t));
+    decent->r = 1.0f;
+    decent->g = 0.0f;
+    decent->b = 0.0f;
+    decent->a = 1.0f;
+    decent->text = "<size=80%>Decent</size>\n";
+    decent->threshold = 60;
+    add(judgements, decent);
+    // Creating Way Off judgement
+    judgement_t* way_off = malloc(sizeof(judgement_t));
+    way_off->r = 0.5f;
+    way_off->g = 0.0f;
+    way_off->b = 0.0f;
+    way_off->a = 1.0f;
+    way_off->text = "<size=80%>Way Off</size>\n";
+    way_off->threshold = 0;
+    add(judgements, way_off);
+
+    // log("Attempting to load judgements from config!");
+    // FILE* config = fopen("/sdcard/Android/data/com.beatgames.beatsaber/files/mods/hitscorevisualizer.cfg", "r");
+
+    // log("Attempting to count number of lines in config!");
+    // int count = 0; // Count of lines, count of judgements.
+    // char c;
+
+    // for (c = getc(config); c != EOF; c = getc(config)) 
+    //     if (c == '\n') // Increment count if this character is newline 
+    //         count = count + 1;
+
+    // log("Found %i lines in config!", count);
+
+    // for (int i = 0; i < count; i++) {
+    //     log("Attempting to load judgement: %i", i);
+    //     // Possible memory leakage?
+    //     float* r = malloc(sizeof(float));
+    //     float* g = malloc(sizeof(float));
+    //     float* b = malloc(sizeof(float));
+    //     float* a = malloc(sizeof(float));
+    //     int* thresh = malloc(sizeof(int));
+    //     char* text;
+    //     // Possible memory leakage?
+    //     judgement* judge = malloc(sizeof(judgement));
+    //     log("Allocated memory for judgement");
+    //     fscanf(config, "%f %f %f %f %i %s", r, g, b, a, thresh, text);
+    //     log("Read judgement from file");
+    //     judge->threshold = *thresh;
+    //     judge->r = *r;
+    //     judge->g = *g;
+    //     judge->b = *b;
+    //     judge->a = *a;
+    //     judge->text = text;
+    //     log("Populated fields of judgement!");
+    //     add(judgements, judge);
+    //     log("Added judge to judgements!");
+    // }
+    // log("Completed reading config!");
+    // fclose(config);
 }
