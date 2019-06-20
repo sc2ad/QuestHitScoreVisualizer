7cdb3f5d3fea7fc49694e22a491e45ded0d98723
Adam ? <adamznow@gmail.com>
2019-06-20 14:19:32 -0700

    Added custom sabers mod

---
+++ customsabers/main.c
@@ -0,0 +1,23 @@
+#include <android/log.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <linux/limits.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+
+#include "../beatsaber-hook/shared/inline-hook/inlineHook.h"
+#include "../beatsaber-hook/shared/utils/utils.h"
+
+
+
+__attribute__((constructor)) void lib_main()
+{
+    log("[CustomSabers] Installing Custom Sabers hooks...");
+
+    log("[CustomSabers] Completed installing hooks!");
+}
