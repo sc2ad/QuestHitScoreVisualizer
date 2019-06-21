a93b030426229578833f0332373895035765a410
Adam ? <adamznow@gmail.com>
2019-06-20 18:04:41 -0700

    Added transparentwalls, probably doesn't work (compile warning)
    Need to eventually move all mods to their own place or rename this repo to be like "scad's quest mod collection" or something

---
+++ transparentwalls/copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
+adb push libs/armeabi-v7a/libtransparentwalls.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libtransparentwalls.so
+adb shell am force-stop com.beatgames.beatsaber
+#adb shell input keyevent KEYCODE_DPAD_RIGHT
+#adb shell input keyevent KEYCODE_SPACE
+
+adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
---
+++ transparentwalls/main.c
@@ -0,0 +1,59 @@
+#include <android/log.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <linux/limits.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+
+#include "../beatsaber-hook/shared/inline-hook/inlineHook.h"
+#include "../beatsaber-hook/shared/utils/utils.h"
+
+#undef log
+#define log(...) __android_log_print(ANDROID_LOG_INFO, "QuestHook", "[TransparentWalls] " __VA_ARGS__)
+
+// MAKE_HOOK(Camera_get_main, 0xC2F6D4, void* camera, void) {
+//     log("Entering Camera.get_main...");
+//     return Camera_get_main();
+// }
+
+MAKE_HOOK(Camera_get_cullingMask, 0xC2DF20, int, void* camera) {
+    log("Entering Camera.get_cullingMask hook...");
+    return Camera_get_cullingMask(camera);
+}
+
+MAKE_HOOK(Camera_set_cullingMask, 0xC2DFB0, void, void* camera, int value) {
+    log("Entering Camera.set_cullingMask hook...");
+    Camera_set_cullingMask(camera, value);
+    log("Completed Camera.set_cullingMask!");
+}
+
+MAKE_HOOK(ObstacleController_ctor, 0x13B0C60, void, void* self) {
+    log("Entering ObstacleController.ctor...");
+    log("Attemping to get Camera...");
+    void* (*fun_ptr)(void) = 0xC2F6D4;
+    void* cam = (*fun_ptr)();
+    log("Attempting to get old culling mask...");
+    int mask = Camera_get_cullingMask(cam);
+    log("Attempting to transform Camera culling mask...");
+    mask |= (1 << 25);
+    log("Attempting to call Camera.set_cullingMask...");
+    Camera_set_cullingMask(cam, mask);
+    log("Completed ObstacleController.ctor!");
+}
+
+__attribute__((constructor)) void lib_main()
+{
+    log("Installing Transparent Walls hooks...");
+    log("Installing ObstacleController.ctor hook!");
+    INSTALL_HOOK(ObstacleController_ctor);
+    log("Installing Camera.get_cullingMask hook!");
+    INSTALL_HOOK(Camera_get_cullingMask);
+    log("Installing Camera.set_cullingMask hook!");
+    INSTALL_HOOK(Camera_set_cullingMask);
+    log("Completed installing hooks!");
+}
