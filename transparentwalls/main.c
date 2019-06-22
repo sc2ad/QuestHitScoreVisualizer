7d5a10e9a647f0019506156166ba4372970d2a36
Adam ? <adamznow@gmail.com>
2019-06-22 00:29:55 -0700

    Transparent walls with proper layer culling mask and possibly set layer
    Still need to figure out LIV camera stuff (I think) before it will work as intended, but it looks like it compiles!

--- transparentwalls/main.c
+++ transparentwalls/main.c
@@ -16,10 +16,28 @@
 #undef log
 #define log(...) __android_log_print(ANDROID_LOG_INFO, "QuestHook", "[TransparentWalls] " __VA_ARGS__)
 
-// MAKE_HOOK(Camera_get_main, 0xC2F6D4, void* camera, void) {
-//     log("Entering Camera.get_main...");
-//     return Camera_get_main();
-// }
+// NOW JUST NEED TO FIGURE OUT THE LIV CAMERA STUFF
+
+
+// INTERESTED IN StrechableCube.Awake: 0x12F05D4
+// WANT TO CALL THE GAMEOBJECT FIELD AND CHANGE ITS LAYER (THIS IS A FUNCTION, SO WE CAN CALL IT!)
+// Component.get_gameObject here: 0xC31C10
+// can take gameObject field and get the field for layer and change it (might even be a property!)
+// IT'S A PROPERTY, YAY! 0xC76FD4
+
+MAKE_HOOK(StretchableCube_Awake, 0x12F05D4, void, void* self) {
+    int layer = 25;
+    log("Entering StretchableCube.Awake hook...");
+    log("Calling orig...");
+    StretchableCube_Awake(self);
+    log("Attempting to get GameObject pointer...");
+    void* (*get_go)(void*) = (void*)getRealOffset(0xC31C10);
+    void* go = (*get_go)(self);
+    log("Attempting to set layer...");
+    void (*set_layer)(void*, int) = (void*)getRealOffset(0xC76FD4);
+    set_layer(go, layer);
+    log("Completed StretchableCube.Awake!");
+}
 
 MAKE_HOOK(Camera_get_cullingMask, 0xC2DF20, int, void* camera) {
     log("Entering Camera.get_cullingMask hook...");
@@ -40,7 +58,7 @@ MAKE_HOOK(ObstacleController_ctor, 0x13B0C60, void, void* self) {
     log("Attempting to get old culling mask...");
     int mask = Camera_get_cullingMask(cam);
     log("Attempting to transform Camera culling mask...");
-    mask |= (1 << 25);
+    mask &= ~(1 << 25);
     log("Attempting to call Camera.set_cullingMask...");
     Camera_set_cullingMask(cam, mask);
     log("Completed ObstacleController.ctor!");
@@ -55,5 +73,7 @@ __attribute__((constructor)) void lib_main()
     INSTALL_HOOK(Camera_get_cullingMask);
     log("Installing Camera.set_cullingMask hook!");
     INSTALL_HOOK(Camera_set_cullingMask);
+    log("Installing StretchableCube.Awake hook!");
+    INSTALL_HOOK(StretchableCube_Awake);
     log("Completed installing hooks!");
 }
