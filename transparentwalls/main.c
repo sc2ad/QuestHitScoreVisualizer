ee5d2375432c2220dbd0550ef4694f9168576efe
Adam ? <adamznow@gmail.com>
2019-06-22 00:47:48 -0700

    Added LIV.ctor hook, hopefully has all the required features now
    I don't know if LIV.ctor is too early for changing the layer mask, I also don't even know if it is needed at all, but in any case, there it is.
    It seems that LayerMask is a struct that only is 4 bytes long, so it should work out as an int (I was too lazy to make it a struct)

--- transparentwalls/main.c
+++ transparentwalls/main.c
@@ -17,7 +17,25 @@
 #define log(...) __android_log_print(ANDROID_LOG_INFO, "QuestHook", "[TransparentWalls] " __VA_ARGS__)
 
 // NOW JUST NEED TO FIGURE OUT THE LIV CAMERA STUFF
-
+// LIV.SpectatorLayerMask foffset: 0x18
+// LIV.ctor: 0x136E7BC
+// Sadly, I don't know exactly how large LayerMask struct is
+// I have the sneaking suspicion that it is larger than 4 bytes, but I could be wrong.
+// Let's try it as 4 bytes for now, with good logging
+// From the Ghidra dump, it looks like it is an undefined 4 (making me believe it is just one 4 byte int)
+// It's also entirely possible that the LIV constructor is not good enough and that I need to do it later
+MAKE_HOOK(LIV_ctor, 0x136E7BC, void, void* self) {
+    log("Entering LIV.ctor hook...");
+    log("Calling orig...");
+    LIV_ctor(self);
+    log("Attempting to get old layer mask...");
+    int toChange = *(int*)(self + 0x18) | (1 << 25);
+    log("Attempting to set new layer to be the OR between current and 25...");
+    *(int*)(self + 0x18) = toChange;
+    log("Attempting to set new layer to be the OR between current and 27...");
+    *(int*)(self + 0x18) = toChange | (1 << 27);
+    log("Completed LIV.ctor!");
+}
 
 // INTERESTED IN StrechableCube.Awake: 0x12F05D4
 // WANT TO CALL THE GAMEOBJECT FIELD AND CHANGE ITS LAYER (THIS IS A FUNCTION, SO WE CAN CALL IT!)
@@ -67,6 +85,8 @@ MAKE_HOOK(ObstacleController_ctor, 0x13B0C60, void, void* self) {
 __attribute__((constructor)) void lib_main()
 {
     log("Installing Transparent Walls hooks...");
+    log("Installing LIV.ctor hook!");
+    INSTALL_HOOK(LIV_ctor);
     log("Installing ObstacleController.ctor hook!");
     INSTALL_HOOK(ObstacleController_ctor);
     log("Installing Camera.get_cullingMask hook!");
