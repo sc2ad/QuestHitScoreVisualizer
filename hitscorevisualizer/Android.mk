4485d7376df7a125c340cab655c204841befc9ae
Adam ? <adamznow@gmail.com>
2019-07-25 02:28:37 -0700

    Updated to 1.5.0, awaiting testing

--- hitscorevisualizer/Android.mk
+++ hitscorevisualizer/Android.mk
@@ -21,12 +21,22 @@ TARGET_ARCH_ABI := armeabi-v7a
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := hook
+
+# include $(CLEAR_VARS)
+# LOCAL_MODULE := testil2cpp
+# LOCAL_SRC_FILES := libil2cpp.so
+# LOCAL_EXPORT_C_INCLUDES := ../beatsaber-hook/shared/libil2cpp
+# include $(PREBUILT_SHARED_LIBRARY)
+
 #LOCAL_SRC_FILES := $(LOCAL_PATH)/../obj/local/armeabi-v7a/libhook.a
 #LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include
 
 include $(CLEAR_VARS)
 LOCAL_LDLIBS := -llog
+LOCAL_CFLAGS    := -DMOD_ID='"QuestHitScoreVisualizer"' -DVERSION='"1.5.0"'
 LOCAL_MODULE    := hitscorevisualizer
-LOCAL_SRC_FILES := main.c ../beatsaber-hook/shared/utils/utils.c ../beatsaber-hook/shared/inline-hook/inlineHook.c ../beatsaber-hook/shared/inline-hook/relocate.c
+# LOCAL_SHARED_LIBRARIES := testil2cpp
+LOCAL_CPPFLAGS := -std=c++17
+LOCAL_SRC_FILES := main.cpp ../beatsaber-hook/shared/utils/utils.cpp ../beatsaber-hook/shared/inline-hook/inlineHook.c ../beatsaber-hook/shared/inline-hook/relocate.c
 #LOCAL_STATIC_LIBRARIES := libhook
 include $(BUILD_SHARED_LIBRARY)
